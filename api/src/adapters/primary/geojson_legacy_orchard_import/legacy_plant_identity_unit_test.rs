e0017e86ed5f527efc713931422d48ec9e2023cf
Valentin Chanas <anisometropie@gmail.com>
2026-08-30 21:50:23 +0200

    test: make legacy identifications explicit

--- api/src/adapters/primary/geojson_legacy_orchard_import/legacy_plant_identity_unit_test.rs
+++ api/src/adapters/primary/geojson_legacy_orchard_import/legacy_plant_identity_unit_test.rs
@@ -12,23 +12,27 @@ fn john_rivers() {
             "Brugnon blanc ‘John Rivers’",
             "Prunus persica var. nucipersica ‘John Rivers’"
         ),
-        identity(
-            "Brugnon blanc",
-            BotanicalTaxon::Named(NamedTaxon {
-                genus: "Prunus".into(),
-                species: Some("persica".into()),
-                species_is_hybrid: false,
-                infraspecific: Some(InfraspecificTaxon {
-                    rank: InfraspecificRank::Variety,
-                    name: "nucipersica".into(),
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Brugnon blanc".into(),
+                botanical_taxon: BotanicalTaxon::Named(NamedTaxon {
+                    genus: "Prunus".into(),
+                    species: Some("persica".into()),
+                    species_is_hybrid: false,
+                    infraspecific: Some(InfraspecificTaxon {
+                        rank: InfraspecificRank::Variety,
+                        name: "nucipersica".into(),
+                    }),
+                    is_aggregate: false,
+                    cultivar_group: None,
                 }),
-                is_aggregate: false,
-                cultivar_group: None,
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "John Rivers".into(),
+                trade_name: None,
             }),
-            Some("John Rivers"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -43,13 +47,17 @@ fn sweet_lifeberry() {
 
     assert_eq!(
         parsed,
-        identity(
-            "Goji",
-            BotanicalTaxon::Named(named_taxon("Lycium", Some("barbarum"))),
-            Some("SMNDSL"),
-            Some("Sweet Lifeberry"),
-            IdentificationStatus::Confirmed,
-        )
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Goji".into(),
+                botanical_taxon: BotanicalTaxon::Named(named_taxon("Lycium", Some("barbarum"),)),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "SMNDSL".into(),
+                trade_name: Some("Sweet Lifeberry".into()),
+            }),
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -64,13 +72,19 @@ fn ombrella() {
 
     assert_eq!(
         parsed,
-        identity(
-            "Albizia",
-            BotanicalTaxon::Named(named_taxon("Albizia", Some("julibrissin"))),
-            Some("Boubri"),
-            Some("Ombrella"),
-            IdentificationStatus::Confirmed,
-        )
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Albizia".into(),
+                botanical_taxon: BotanicalTaxon::Named(
+                    named_taxon("Albizia", Some("julibrissin"),)
+                ),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Boubri".into(),
+                trade_name: Some("Ombrella".into()),
+            }),
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -81,20 +95,24 @@ fn thornfree() {
             "Mûre sans épines ‘Thornfree’",
             "Rubus fruticosus agg. ‘Thornfree’"
         ),
-        identity(
-            "Mûre sans épines",
-            BotanicalTaxon::Named(NamedTaxon {
-                genus: "Rubus".into(),
-                species: Some("fruticosus".into()),
-                species_is_hybrid: false,
-                infraspecific: None,
-                is_aggregate: true,
-                cultivar_group: None,
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Mûre sans épines".into(),
+                botanical_taxon: BotanicalTaxon::Named(NamedTaxon {
+                    genus: "Rubus".into(),
+                    species: Some("fruticosus".into()),
+                    species_is_hybrid: false,
+                    infraspecific: None,
+                    is_aggregate: true,
+                    cultivar_group: None,
+                }),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Thornfree".into(),
+                trade_name: None,
             }),
-            Some("Thornfree"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -105,20 +123,24 @@ fn emil() {
             "Myrtillier ‘Emil’",
             "Vaccinium (Angustifolium Group) ‘Emil’"
         ),
-        identity(
-            "Myrtillier",
-            BotanicalTaxon::Named(NamedTaxon {
-                genus: "Vaccinium".into(),
-                species: None,
-                species_is_hybrid: false,
-                infraspecific: None,
-                is_aggregate: false,
-                cultivar_group: Some("Angustifolium".into()),
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Myrtillier".into(),
+                botanical_taxon: BotanicalTaxon::Named(NamedTaxon {
+                    genus: "Vaccinium".into(),
+                    species: None,
+                    species_is_hybrid: false,
+                    infraspecific: None,
+                    is_aggregate: false,
+                    cultivar_group: Some("Angustifolium".into()),
+                }),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Emil".into(),
+                trade_name: None,
             }),
-            Some("Emil"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -129,20 +151,24 @@ fn medana_tayberry() {
             "Mûre-framboise ‘Medana Tayberry’",
             "Rubus (Tayberry Group) ‘Medana Tayberry’",
         ),
-        identity(
-            "Mûre-framboise",
-            BotanicalTaxon::Named(NamedTaxon {
-                genus: "Rubus".into(),
-                species: None,
-                species_is_hybrid: false,
-                infraspecific: None,
-                is_aggregate: false,
-                cultivar_group: Some("Tayberry".into()),
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Mûre-framboise".into(),
+                botanical_taxon: BotanicalTaxon::Named(NamedTaxon {
+                    genus: "Rubus".into(),
+                    species: None,
+                    species_is_hybrid: false,
+                    infraspecific: None,
+                    is_aggregate: false,
+                    cultivar_group: Some("Tayberry".into()),
+                }),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Medana Tayberry".into(),
+                trade_name: None,
             }),
-            Some("Medana Tayberry"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -153,20 +179,24 @@ fn bredon_springs() {
             "Lavatère arbustive ‘Bredon Springs’",
             "Malva × clementii ‘Bredon Springs’",
         ),
-        identity(
-            "Lavatère arbustive",
-            BotanicalTaxon::Named(NamedTaxon {
-                genus: "Malva".into(),
-                species: Some("clementii".into()),
-                species_is_hybrid: true,
-                infraspecific: None,
-                is_aggregate: false,
-                cultivar_group: None,
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Lavatère arbustive".into(),
+                botanical_taxon: BotanicalTaxon::Named(NamedTaxon {
+                    genus: "Malva".into(),
+                    species: Some("clementii".into()),
+                    species_is_hybrid: true,
+                    infraspecific: None,
+                    is_aggregate: false,
+                    cultivar_group: None,
+                }),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Bredon Springs".into(),
+                trade_name: None,
             }),
-            Some("Bredon Springs"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -174,18 +204,22 @@ fn bredon_springs() {
 fn baco_noir() {
     assert_eq!(
         parse("Baco Noir", "Vitis vinifera × Vitis riparia ‘Baco Noir’"),
-        identity(
-            "Baco Noir",
-            BotanicalTaxon::HybridFormula {
-                parents: [
-                    named_taxon("Vitis", Some("vinifera")),
-                    named_taxon("Vitis", Some("riparia")),
-                ],
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Baco Noir".into(),
+                botanical_taxon: BotanicalTaxon::HybridFormula {
+                    parents: [
+                        named_taxon("Vitis", Some("vinifera")),
+                        named_taxon("Vitis", Some("riparia")),
+                    ],
+                },
             },
-            Some("Baco Noir"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Baco Noir".into(),
+                trade_name: None,
+            }),
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -193,13 +227,17 @@ fn baco_noir() {
 fn worcesterberry() {
     assert_eq!(
         parse("Groseillier ‘Worcesterberry’", "Ribes ‘Worcesterberry’"),
-        identity(
-            "Groseillier",
-            BotanicalTaxon::Named(named_taxon("Ribes", None)),
-            Some("Worcesterberry"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Groseillier".into(),
+                botanical_taxon: BotanicalTaxon::Named(named_taxon("Ribes", None)),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Worcesterberry".into(),
+                trade_name: None,
+            }),
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -207,13 +245,14 @@ fn worcesterberry() {
 fn prune_totoche() {
     assert_eq!(
         parse("Prune totoche", "Prunus domestica ?"),
-        identity(
-            "Prune totoche",
-            BotanicalTaxon::Named(named_taxon("Prunus", Some("domestica"))),
-            None,
-            None,
-            IdentificationStatus::Uncertain,
-        )
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Prune totoche".into(),
+                botanical_taxon: BotanicalTaxon::Named(named_taxon("Prunus", Some("domestica"),)),
+            },
+            plant_cultivar: None,
+            identification_status: IdentificationStatus::Uncertain,
+        }
     );
 }
 
@@ -221,23 +260,24 @@ fn prune_totoche() {
 fn pin_noir() {
     assert_eq!(
         parse("Pin noir d’Autriche", "Pinus nigra subsp. nigra"),
-        identity(
-            "Pin noir d’Autriche",
-            BotanicalTaxon::Named(NamedTaxon {
-                genus: "Pinus".into(),
-                species: Some("nigra".into()),
-                species_is_hybrid: false,
-                infraspecific: Some(InfraspecificTaxon {
-                    rank: InfraspecificRank::Subspecies,
-                    name: "nigra".into(),
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Pin noir d’Autriche".into(),
+                botanical_taxon: BotanicalTaxon::Named(NamedTaxon {
+                    genus: "Pinus".into(),
+                    species: Some("nigra".into()),
+                    species_is_hybrid: false,
+                    infraspecific: Some(InfraspecificTaxon {
+                        rank: InfraspecificRank::Subspecies,
+                        name: "nigra".into(),
+                    }),
+                    is_aggregate: false,
+                    cultivar_group: None,
                 }),
-                is_aggregate: false,
-                cultivar_group: None,
-            }),
-            None,
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            },
+            plant_cultivar: None,
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -245,13 +285,17 @@ fn pin_noir() {
 fn goutte_dor() {
     assert_eq!(
         parse("Figuier ‘Goutte d’Or’", "Ficus carica ‘Goutte d’Or’"),
-        identity(
-            "Figuier",
-            BotanicalTaxon::Named(named_taxon("Ficus", Some("carica"))),
-            Some("Goutte d’Or"),
-            None,
-            IdentificationStatus::Confirmed,
-        )
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Figuier".into(),
+                botanical_taxon: BotanicalTaxon::Named(named_taxon("Ficus", Some("carica"))),
+            },
+            plant_cultivar: Some(PlantCultivar {
+                cultivar: "Goutte d’Or".into(),
+                trade_name: None,
+            }),
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -259,13 +303,17 @@ fn goutte_dor() {
 fn pistachier_terebinthe() {
     assert_eq!(
         parse("Pistachier térébinthe", "Pistacia terebinthus"),
-        identity(
-            "Pistachier térébinthe",
-            BotanicalTaxon::Named(named_taxon("Pistacia", Some("terebinthus"))),
-            None,
-            None,
-            IdentificationStatus::Confirmed,
-        )
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Pistachier térébinthe".into(),
+                botanical_taxon: BotanicalTaxon::Named(named_taxon(
+                    "Pistacia",
+                    Some("terebinthus"),
+                )),
+            },
+            plant_cultivar: None,
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
@@ -276,48 +324,29 @@ fn kamtschatica() {
             "Camérisier du Kamtchatka",
             "Lonicera caerulea var. kamtschatica",
         ),
-        identity(
-            "Camérisier du Kamtchatka",
-            BotanicalTaxon::Named(NamedTaxon {
-                genus: "Lonicera".into(),
-                species: Some("caerulea".into()),
-                species_is_hybrid: false,
-                infraspecific: Some(InfraspecificTaxon {
-                    rank: InfraspecificRank::Variety,
-                    name: "kamtschatica".into(),
+        PlantIdentification {
+            plant_identity: PlantIdentity {
+                common_name: "Camérisier du Kamtchatka".into(),
+                botanical_taxon: BotanicalTaxon::Named(NamedTaxon {
+                    genus: "Lonicera".into(),
+                    species: Some("caerulea".into()),
+                    species_is_hybrid: false,
+                    infraspecific: Some(InfraspecificTaxon {
+                        rank: InfraspecificRank::Variety,
+                        name: "kamtschatica".into(),
+                    }),
+                    is_aggregate: false,
+                    cultivar_group: None,
                 }),
-                is_aggregate: false,
-                cultivar_group: None,
-            }),
-            None,
-            None,
-            IdentificationStatus::Confirmed,
-        )
+            },
+            plant_cultivar: None,
+            identification_status: IdentificationStatus::Confirmed,
+        }
     );
 }
 
 fn parse(name: &str, latin_name: &str) -> PlantIdentification {
     parse_legacy_tree_identity(name, latin_name, None).unwrap()
-}
-
-fn identity(
-    common_name: &str,
-    botanical_taxon: BotanicalTaxon,
-    cultivar: Option<&str>,
-    trade_name: Option<&str>,
-    identification_status: IdentificationStatus,
-) -> PlantIdentification {
-    PlantIdentification {
-        plant_identity: PlantIdentity {
-            common_name: common_name.into(),
-            botanical_taxon,
-        },
-        plant_cultivar: cultivar.map(|cultivar| PlantCultivar {
-            cultivar: cultivar.into(),
-            trade_name: trade_name.map(str::to_owned),
-        }),
-        identification_status,
-    }
 }
 
 fn named_taxon(genus: &str, species: Option<&str>) -> NamedTaxon {
